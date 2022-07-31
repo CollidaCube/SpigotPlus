fa0577d29eef14dd1043dd2eda71831abfd9c355
CollidaCube <spam@collidacube.com>
2022-07-30 19:55:09 -0700

    Backup [#1]
    Last one didn't do the thing..?
    Initialized project in IntelliJ

---
+++ src/main/java/com/collidacube/plugins/spigotplus/SpigotPlus.java
@@ -0,0 +1,17 @@
+package com.collidacube.plugins.spigotplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
